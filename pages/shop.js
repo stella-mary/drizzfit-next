8ccc3e46cde1af9be770f5652d3ec2a2540d5268
stella-mary <mariastellamary@outlook.com>
2023-08-18 16:38:02 +0530

    shop js added

---
+++ pages/shop.js
@@ -0,0 +1,11 @@
+import Shop from '@/components/Shop/Shop'
+
+const shop = () => {
+    return (
+        <div className="Project_Container">
+            <Shop />
+        </div>
+    );
+};
+
+export default shop;
