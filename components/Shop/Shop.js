c85b9ef71083dd11a76bbe97c16cab4cbd111808
stella-mary <mariastellamary@outlook.com>
2023-08-18 19:29:39 +0530

    add shop  js

--- components/Shop/Shop.js
+++ components/Shop/Shop.js
@@ -1,108 +1,42 @@
-import styles from "@/styles/Promotion.module.css"
+import styles from "@/styles/Shop.module.css"
 import { useRouter } from "next/router"
 import Image from "next/image";
 import { motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
-const boxVariant = {
-    visible: {
-        opacity: 1,
-        x: 0, // Change from y: 0 to x: 0
-        transition: { duration: 0.5 },
-    },
-    hidden: { opacity: 0, x: -50 }, // Change from y: 50 to x: -50
-};
+// const boxVariant = {
+//     visible: {
+//         opacity: 1,
+//         x: 0, // Change from y: 0 to x: 0
+//         transition: { duration: 0.5 },
+//     },
+//     hidden: { opacity: 0, x: -50 }, // Change from y: 50 to x: -50
+// };
 
 
-const Promotion = () => {
-
-    const router = useRouter();
-
-    const navigateToShop = () => {
-        router.push("/shop");
-    };
-
-    const control1 = useAnimation();
-    const [ref1, inView1] = useInView();
-    const control2 = useAnimation();
-    const [ref2, inView2] = useInView();
-    const control3 = useAnimation();
-    const [ref3, inView3] = useInView();
-
-    useEffect(() => {
-        if (inView1) {
-            control1.start("visible");
-        } else {
-            control1.start("hidden");
-        }
-    }, [control1, inView1]);
-
-    useEffect(() => {
-        if (inView2) {
-            control2.start("visible");
-        } else {
-            control2.start("hidden");
-        }
-    }, [control2, inView2]);
-
-    useEffect(() => {
-        if (inView3) {
-            control3.start("visible");
-        } else {
-            control3.start("hidden");
-        }
-    }, [control3, inView3]);
+const Shop = () => {
 
     return (
-        <div className={styles.promotion}>
-            <div className={styles.promotionMain} >
-                <motion.div
-                    className={styles.h1}
-                    ref={ref1}
-                    variants={boxVariant}
-                    initial="hidden"
-                    animate={control1}
-                >
-                    Get a mess free periods!
-                </motion.div>
-                <motion.div
-                    className={styles.para}
-                    ref={ref2}
-                    variants={boxVariant}
-                    initial="hidden"
-                    animate={control2}
-                >
-                    Switch to menstrual cup and forget
-                    <br />
-                    your periods forever.
-                </motion.div>
-                <motion.div
-                    className={styles.button}
-                    ref={ref3}
-                    variants={boxVariant}
-                    initial="hidden"
-                    animate={control3}
-                    onClick={navigateToShop}
-                >
-                    Shop
-                </motion.div>
+        <div className={styles.shop}>
+            <div className={styles.shopMain}>
+                <div className={styles.imageContainer1}></div>
+                <div className={styles.shopMain1}>
+                    <div className={styles.imageContainer2}></div>
+                </div>
+                <div className={styles.shopMain2}>
+                    <div className={styles.h1}>Metabo – Immunity Booster</div>
+                    SKU: N/A
+                    From: ₹999.00
+                    Enter Pincode
+                    CHECK PINCODE
+                    Metabo keeps your immune system strong. It provides proper healthy rich nutrients to your lifestyle. Improve your digestion and metabolism with our immunity booster.
+                    Free Shipping on all Prepaid orders.
+
+                </div>
             </div>
-            <div className={styles.imageContainer1}>
-            </div>
-            {/* <Image
-                src="/image/promotion.png"
-                alt="promotion"
-                sizes="100vw"
-                style={{
-                    width: "100%",
-                    height: "100vh",
-                }}
-                width={500}
-                height={100}
-            /> */}
         </div>
     )
 }
 
-export default Promotion
+export default Shop
