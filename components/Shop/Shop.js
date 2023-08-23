be42de6b8f6d2db8c6125c52dc74f158b7a3eff8
stella-mary <mariastellamary@outlook.com>
2023-08-23 11:56:02 +0530

    shop js complete

--- components/Shop/Shop.js
+++ components/Shop/Shop.js
@@ -1,25 +1,46 @@
+import React, { useState } from "react";
 import styles from "@/styles/Shop.module.css"
 import { Box, Typography, useTheme } from "@mui/material";
 import { Facebook, Instagram, Twitter } from '@mui/icons-material';
-import Image from "next/image";
 import { useRouter } from "next/router"
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default function Shop() {
+export default function Buy() {
+
+    const router = useRouter();
+
+    const navigateToBuy = () => {
+        router.push("/buy");
+    };
+
+
+    const [selectedQuantity, setSelectedQuantity] = useState(1);
+
+    const handleDecreaseQuantity = () => {
+        if (selectedQuantity > 1) {
+            setSelectedQuantity(selectedQuantity - 1);
+        }
+    };
+
+    const handleIncreaseQuantity = () => {
+        setSelectedQuantity(selectedQuantity + 1);
+    };
 
     return (
         <div className={styles.shop}>
             <Box
                 display="flex"
                 width="97%"
+                height="100vh"
                 alignItems="top"
                 gap="1%"
                 borderRadius="10px"
                 paddingLeft="1%"
                 paddingTop='1%'
                 paddingRight='1%'
-                m="20px"
+                m="10px"
                 backgroundColor="#fee600"
             >
                 <Box
@@ -30,14 +51,9 @@ export default function Shop() {
                         width: "10%",
                     }}
                 >
-
-                    <div className={styles.shopContainer2}>
-                    </div>
-                    <div className={styles.shopContainer3}>
-                    </div>
-                    <div className={styles.shopContainer4}>
-                    </div>
-
+                    <div className={styles.shopContainer2}></div>
+                    <div className={styles.shopContainer3}></div>
+                    <div className={styles.shopContainer4}></div>
                 </Box>
 
                 <Box
@@ -49,14 +65,10 @@ export default function Shop() {
                     }}
                 >
                     <div className={styles.productDetails}>
-                        <div className={styles.shopContainer5}>
-                        </div>
-
+                        <div className={styles.shopContainer5}></div>
                         <div className={styles.heartIcon}>
                             <FavoriteBorderIcon sx={{ fontSize: "40px", color: "black", textAlign: 'center' }} />
-
                         </div>
-
                     </div>
                 </Box>
 
@@ -67,15 +79,44 @@ export default function Shop() {
                         alignItems: "top",
                     }}
                 >
-                    <Typography backgroundColor="black" color="white" padding="5px" width="25%" borderRadius="10px" textAlign="center">In Stock</Typography>
-                    <Typography variant="h5" color="#8ca3ba" marginTop="20px">NIKE</Typography>
-                    <Typography variant="h3" marginTop="10px"><b>Air Jordan 270</b></Typography>
-                    <Typography variant="h3" marginTop="10px" color="#2499ef">$350</Typography>
+                    <Typography
+                        backgroundColor="black"
+                        color="white"
+                        padding="5px"
+                        width="25%"
+                        borderRadius="10px"
+                        textAlign="center"
+                        style={{ fontFamily: "'Telegraf UltraBold 800', sans-serif" }}
+                    >
+                        In Stock
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        color="black"
+                        marginTop="20px"
+                        style={{ fontFamily: "'Telegraf UltraBold 800', sans-serif" }}
+                    >
+                        NIKE
+                    </Typography>
+                    <Typography
+                        variant="h3"
+                        marginTop="10px"
+                        style={{ fontFamily: "'Telegraf UltraBold 800', sans-serif" }}
+                    >
+                        <b>Air Jordan 270</b>
+                    </Typography>
+                    <Typography
+                        variant="h3"
+                        marginTop="10px"
+                        color="black"
+                        style={{ fontFamily: "'Telegraf UltraBold 800', sans-serif" }}
+                    >
+                        $350
+                    </Typography>
 
-
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', gap: '10px' }}>
-                        <label htmlFor="sizeSelect" style={{ marginRight: '10px' }}>
-                            <b>Select Size:</b>
+                    <div style={{ display: 'flex', alignItems: 'center', fontFamily: "'Telegraf Regular 400', sans-serif", marginTop: '20px', gap: '10px' }}>
+                        <label htmlFor="sizeSelect" style={{ marginRight: '10px', fontSize: '20px', fontFamily: "'Telegraf Regular 400', sans-serif" }}>
+                            Select Size:
                         </label>
                         <select
                             id="sizeSelect"
@@ -84,42 +125,41 @@ export default function Shop() {
                             style={{
                                 flex: '1',
                                 maxWidth: '70px',
-                                color: 'white',
+                                color: 'black',
                                 backgroundColor: "transparent",
                                 padding: '10px',
-                                borderColor: "#3d454e",
+                                fontFamily: "'Telegraf UltraBold 800', sans-serif",
                                 borderWidth: '2px',
                                 borderStyle: 'solid'
                             }}
                         >
-
-                            <option style={{ color: 'black' }} value="M">M</option>
-                            <option style={{ color: 'black' }} value="S">S</option>
-                            <option style={{ color: 'black' }} value="L">L</option>
+                            <option style={{ color: 'black', fontFamily: "'Telegraf Regular 400', sans-serif" }} value="M">M</option>
+                            <option style={{ color: 'black', fontFamily: "'Telegraf Regular 400', sans-serif" }} value="S">S</option>
+                            <option style={{ color: 'black', fontFamily: "'Telegraf Regular 400', sans-serif" }} value="L">L</option>
                         </select>
                         <style>{`
           select#sizeSelect option:checked {
-            border-color: #3d454e;
+            border-color: black;
           }
         `}</style>
                     </div>
 
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', gap: '20px' }}>
-                        <Typography variant="h6" marginTop="20px"><b>Quantity:</b></Typography>
+                    <div style={{ display: 'flex', alignItems: 'center', fontFamily: "'Telegraf Regular 400', sans-serif", marginTop: '20px', gap: '20px' }}>
+                        <Typography variant="h6" marginTop="20px" style={{ fontFamily: "'Telegraf Regular 400', sans-serif" }}>Quantity:</Typography>
                         <div>
                             <div
                                 style={{
                                     display: 'flex',
                                     alignItems: 'center',
                                     justifyContent: 'center',
-                                    variant: 'h3',
-                                    fontWeight: 400,
                                     fontSize: '15px',
                                     marginTop: '15px',
-                                    border: '2px solid #2f4264',
+                                    border: '2px solid black',
                                     borderRadius: '6px',
-                                    padding: '5px',
+                                    padding: '10px',
                                     width: '100px',
+                                    height: '40px',
+                                    fontFamily: "'Telegraf Regular 400', sans-serif"
                                 }}
                             >
                                 <button
@@ -130,16 +170,18 @@ export default function Shop() {
                                         cursor: 'pointer',
                                         marginRight: '5px',
                                         fontSize: '15px',
-                                        color: 'white',
+                                        color: 'black',
+                                        fontFamily: "'Telegraf Regular 400', sans-serif"
                                     }}
+                                    onClick={handleDecreaseQuantity}
 
-                                    onClick={() => {
-                                        setSelectedQuantity(selectedQuantity - 1)
-                                    }}
+                                // onClick={() => {
+                                //     setSelectedQuantity(selectedQuantity - 1)
+                                // }}
                                 >
                                     -
                                 </button>
-                                {/* <span style={{ flex: 1, textAlign: 'center', color: 'white' }}>{selectedQuantity}</span> */}
+                                <span style={{ flex: 1, textAlign: 'center', color: 'black', fontFamily: "'Telegraf Regular 400', sans-serif" }}>{selectedQuantity}</span>
                                 <button
                                     style={{
                                         border: 'none',
@@ -147,12 +189,14 @@ export default function Shop() {
                                         cursor: 'pointer',
                                         marginLeft: '5px',
                                         fontSize: '15px',
-                                        color: 'white',
+                                        color: 'black',
+                                        fontFamily: "'Telegraf Regular 400', sans-serif"
                                     }}
+                                    onClick={handleIncreaseQuantity}
 
-                                    onClick={() => {
-                                        setSelectedQuantity(selectedQuantity + 1)
-                                    }}
+                                // onClick={() => {
+                                //     setSelectedQuantity(selectedQuantity + 1)
+                                // }}
                                 >
                                     +
                                 </button>
@@ -161,29 +205,25 @@ export default function Shop() {
 
                         </div>
                     </div>
-                    <Typography variant="h6" marginTop="20px">
-                        <b>
-                            <span className="color21">Available: 12</span>
-                        </b>
+                    <Typography variant="h6" marginTop="15px" style={{ fontFamily: "'Telegraf Regular 400', sans-serif" }}>
+                        <span className="color21">Available: 12</span>
                     </Typography>
-
 
                     <Box
                         display="flex"
-                        justifyContent="-moz-initial"
                         marginTop="40px"
                         gap="20px"
-
+                        fontFamily="'Telegraf Regular 400', sans-serif"
                     >
-                        <Typography fontSize="10px" >
-                            <span class="bgcolor6">
+                        <Typography fontSize="15px">
+                            <span class={styles.bgcolor5}>
                                 {/* onClick={handleAddToCart} */}
-                                <b>Add to cart</b>
+                                Add to cart
                             </span>
                         </Typography>
-                        <Typography fontSize="10px" >
-                            <span class="bgcolor5">
-                                <b>Buy Now</b>
+                        <Typography fontSize="15px">
+                            <span class={styles.bgcolor5} onClick={navigateToBuy}>
+                                Buy Now
                             </span>
                         </Typography>
                     </Box>
@@ -195,19 +235,19 @@ export default function Shop() {
                         gap="10px"
                     >
 
-                        <div className="social-icon">
-                            <span><FontAwesomeIcon /></span>
+                        <div className={styles.socialIcon}>
+                            <span><FontAwesomeIcon icon={faFacebookF} /></span>
                         </div>
-                        <div className="social-icon">
+                        <div className={styles.socialIcon}>
                             <span><Instagram /></span>
                         </div>
-                        <div className="social-icon">
+                        <div className={styles.socialIcon}>
                             <span><Twitter /></span>
                         </div>
 
                     </Box>
                 </Box>
             </Box >
-        </div>
+        </div >
     );
 }
---
+++ pages/buy.js
@@ -0,0 +1,12 @@
+import Buy from '@/components/Buy/Buy'
+import styles from '../styles/Buy.module.css'
+
+const shop = () => {
+    return (
+        <div className={styles.container}>
+            <Buy />
+        </div>
+    );
+};
+
+export default shop;
