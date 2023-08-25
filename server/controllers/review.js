bf7c535f90a0111d5806a7c1444928315d3c15f4
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-08-25 19:18:49 +0530

    Create review.js

---
+++ server/controllers/review.js
@@ -0,0 +1,26 @@
+import Reviews from "../models/Reviews.js";
+
+export const postReview = (req, res) => {
+  const customerId = req.body.customerId;
+  const rating = req.body.rating;
+  const reviewDetails = req.body.reviewDetails;
+  const reviewHeading = req.body.reviewHeading;
+
+  const newReview = new Reviews({
+    customerId,
+    rating,
+    reviewDetails,
+    reviewHeading,
+  });
+
+  newReview
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json("Error: ", err));
+};
+
+export const getAllReviews = (req, res) => {
+  Reviews.find()
+    .then((reviews) => res.json(reviews))
+    .catch((err) => res.status(400).json("Error: ", err));
+};
