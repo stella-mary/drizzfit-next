a230849ac3ca16da1a2d4572268b9779dffecbc8
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-08-25 18:43:58 +0530

    Create Reviews.js

---
+++ server/models/Reviews.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import autoIncrement from "mongoose-plugin-autoinc";
+
+const reviewSchema = new mongoose.Schema({
+  reviewId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  customerId: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  reviewHeading: {
+    type: String,
+    required: true,
+  },
+  reviewDetails: {
+    type: String,
+    required: true,
+  },
+});
+
+reviewSchema.plugin(autoIncrement.plugin, {
+  model: "DrizzfitReviews",
+  field: "reviewId",
+  startAt: 1,
+  incrementBy: 1,
+});
+
+const Reviews = mongoose.model("DrizzfitReviews", reviewSchema);
+
+export default Reviews;
