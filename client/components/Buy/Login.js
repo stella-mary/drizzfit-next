4896e367336dc29d7cbb671d66f863658ba5d9c6
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-08-26 18:29:14 +0530

    Name Changed

--- client/components/Buy/Login.js
+++ client/components/Buy/Login.js
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 
 // Material UI Imports
 import {
-    TextField,
-    InputAdornment,
-    FormControl,
-    InputLabel,
-    IconButton,
-    Button,
-    Input,
-    Checkbox,
-    Alert,
-    Stack,
+  TextField,
+  InputAdornment,
+  FormControl,
+  InputLabel,
+  IconButton,
+  Button,
+  Input,
+  Checkbox,
+  Alert,
+  Stack,
 } from "@mui/material";
 
 import Visibility from "@mui/icons-material/Visibility";
@@ -20,190 +20,187 @@ import LoginIcon from "@mui/icons-material/Login";
 
 // Email Validation
 const isEmail = (email) =>
-    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
 
 export default function Login() {
-    const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
 
-    //Inputs
-    const [emailInput, setEmailInput] = useState();
-    const [passwordInput, setPasswordInput] = useState();
-    const [rememberMe, setRememberMe] = useState();
+  //Inputs
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [rememberMe, setRememberMe] = useState();
 
-    // Inputs Errors
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
+  // Inputs Errors
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
-    // Overall Form Validity
-    const [formValid, setFormValid] = useState();
-    const [success, setSuccess] = useState();
+  // Overall Form Validity
+  const [formValid, setFormValid] = useState();
+  const [success, setSuccess] = useState();
 
-    // Handles Display and Hide Password
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+  // Handles Display and Hide Password
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-    // Label for Checkbox
-    const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  // Label for Checkbox
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-    // Validation for onBlur Email
-    const handleEmail = () => {
-        console.log(isEmail(emailInput));
-        if (!isEmail(emailInput)) {
-            setEmailError(true);
-            return;
-        }
+  // Validation for onBlur Email
+  const handleEmail = () => {
+    console.log(isEmail(email));
+    if (!isEmail(email)) {
+      setEmailError(true);
+      return;
+    }
 
-        setEmailError(false);
-    };
+    setEmailError(false);
+  };
 
-    // Validation for onBlur Password
-    const handlePassword = () => {
-        if (
-            !passwordInput ||
-            passwordInput.length < 5 ||
-            passwordInput.length > 20
-        ) {
-            setPasswordError(true);
-            return;
-        }
+  // Validation for onBlur Password
+  const handlePassword = () => {
+    if (!password || password.length < 5 || password.length > 20) {
+      setPasswordError(true);
+      return;
+    }
 
-        setPasswordError(false);
-    };
+    setPasswordError(false);
+  };
 
-    //handle Submittion
-    const handleSubmit = () => {
-        setSuccess(null);
-        //First of all Check for Errors
+  //handle Submittion
+  const handleSubmit = () => {
+    setSuccess(null);
+    //First of all Check for Errors
 
-        // If Email error is true
-        if (emailError || !emailInput) {
-            setFormValid("Email is Invalid. Please Re-Enter");
-            return;
-        }
+    // If Email error is true
+    if (emailError || !email) {
+      setFormValid("Email is Invalid. Please Re-Enter");
+      return;
+    }
 
-        // If Password error is true
-        if (passwordError || !passwordInput) {
-            setFormValid(
-                "Password is set btw 5 - 20 characters long. Please Re-Enter"
-            );
-            return;
-        }
-        setFormValid(null);
+    // If Password error is true
+    if (passwordError || !password) {
+      setFormValid(
+        "Password is set btw 5 - 20 characters long. Please Re-Enter"
+      );
+      return;
+    }
+    setFormValid(null);
 
-        // Proceed to use the information passed
-        console.log("Email : " + emailInput);
-        console.log("Password : " + passwordInput);
-        console.log("Remember : " + rememberMe);
+    // Proceed to use the information passed
+    console.log("Email : " + email);
+    console.log("Password : " + password);
+    console.log("Remember : " + rememberMe);
 
-        //Show Successfull Submittion
-        setSuccess("Form Submitted Successfully");
-    };
+    //Show Successfull Submittion
+    setSuccess("Form Submitted Successfully");
+  };
 
-    return (
-        <div>
-            <div style={{ marginTop: "5px" }}>
-                <TextField
-                    label="Email Address"
-                    fullWidth
-                    error={emailError}
-                    id="standard-basic"
-                    variant="standard"
-                    sx={{ width: "100%" }}
-                    value={emailInput}
-                    InputProps={{}}
-                    size="small"
-                    onBlur={handleEmail}
-                    onChange={(event) => {
-                        setEmailInput(event.target.value);
-                    }}
-                    InputLabelProps={{
-                        style: { color: 'white' } // Apply white color to the label
-                    }}
-                />
-            </div>
-            <div style={{ marginTop: "5px" }}>
-                <FormControl sx={{ width: "100%" }} variant="standard">
-                    <InputLabel
-                        error={passwordError}
-                        htmlFor="standard-adornment-password"
-                        style={{ color: 'white', borderColor: 'white' }}
-
-
-                    >
-                        Password
-                    </InputLabel>
-                    <Input
-                        error={passwordError}
-                        onBlur={handlePassword}
-                        id="standard-adornment-password"
-                        type={showPassword ? "text" : "password"}
-                        onChange={(event) => {
-                            setPasswordInput(event.target.value);
-                        }}
-                        value={passwordInput}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                >
-                                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-            </div>
-
-            <div style={{ fontSize: "10px" }}>
-                <Checkbox
-                    {...label}
-                    size="small"
-                    onChange={(event) => setRememberMe(event.target.checked)}
-                />
-                Remember Me
-            </div>
-
-            <div style={{ marginTop: "10px" }}>
-                <Button
-                    variant="contained"
-                    fullWidth
-                    startIcon={<LoginIcon />}
-                    onClick={handleSubmit}
+  return (
+    <div>
+      <div style={{ marginTop: "5px" }}>
+        <TextField
+          label="Email Address"
+          fullWidth
+          error={emailError}
+          id="standard-basic"
+          variant="standard"
+          sx={{ width: "100%" }}
+          value={email}
+          InputProps={{}}
+          size="small"
+          onBlur={handleEmail}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          InputLabelProps={{
+            style: { color: "white" }, // Apply white color to the label
+          }}
+        />
+      </div>
+      <div style={{ marginTop: "5px" }}>
+        <FormControl sx={{ width: "100%" }} variant="standard">
+          <InputLabel
+            error={passwordError}
+            htmlFor="standard-adornment-password"
+            style={{ color: "white", borderColor: "white" }}
+          >
+            Password
+          </InputLabel>
+          <Input
+            error={passwordError}
+            onBlur={handlePassword}
+            id="standard-adornment-password"
+            type={showPassword ? "text" : "password"}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            value={password}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
                 >
-                    LOGIN
-                </Button>
-            </div>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
 
-            {/* Show Form Error if any */}
-            {formValid && (
-                <Stack sx={{ width: "100%", paddingTop: "10px" }} spacing={2}>
-                    <Alert severity="error" size="small">
-                        {formValid}
-                    </Alert>
-                </Stack>
-            )}
+      <div style={{ fontSize: "10px" }}>
+        <Checkbox
+          {...label}
+          size="small"
+          onChange={(event) => setRememberMe(event.target.checked)}
+        />
+        Remember Me
+      </div>
 
-            {/* Show Success if no issues */}
-            {success && (
-                <Stack sx={{ width: "100%", paddingTop: "10px" }} spacing={2}>
-                    <Alert severity="success" size="small">
-                        {success}
-                    </Alert>
-                </Stack>
-            )}
+      <div style={{ marginTop: "10px" }}>
+        <Button
+          variant="contained"
+          fullWidth
+          startIcon={<LoginIcon />}
+          onClick={handleSubmit}
+        >
+          LOGIN
+        </Button>
+      </div>
 
-            <div style={{ marginTop: "7px", fontSize: "10px", color: 'white' }} margin="left">
-                <a>Forgot Password</a>
-                <br />
-                Do you have an account ?{" "}
-                <small style={{ textDecoration: "underline", color: "blue" }}>
-                    Sign Up
-                </small>
-            </div>
-        </div>
-    );
+      {/* Show Form Error if any */}
+      {formValid && (
+        <Stack sx={{ width: "100%", paddingTop: "10px" }} spacing={2}>
+          <Alert severity="error" size="small">
+            {formValid}
+          </Alert>
+        </Stack>
+      )}
+
+      {/* Show Success if no issues */}
+      {success && (
+        <Stack sx={{ width: "100%", paddingTop: "10px" }} spacing={2}>
+          <Alert severity="success" size="small">
+            {success}
+          </Alert>
+        </Stack>
+      )}
+
+      <div
+        style={{ marginTop: "7px", fontSize: "10px", color: "white" }}
+        margin="left"
+      >
+        <a>Forgot Password</a>
+        <br />
+        Do you have an account ?{" "}
+        <small style={{ textDecoration: "underline", color: "blue" }}>
+          Sign Up
+        </small>
+      </div>
+    </div>
+  );
 }
