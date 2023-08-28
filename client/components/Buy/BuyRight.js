ba309b8ba6ae68758c6077cd872cc0be291dab6c
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-08-28 16:18:10 +0530

    Update BuyRight.js

--- client/components/Buy/BuyRight.js
+++ client/components/Buy/BuyRight.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
 import styles from "@/styles/BuyRight.module.css";
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
+import axios from "axios";
 
 const BuyRight = () => {
-    return (
-        <div className={styles.BuyRightContainer}>
-            <div className={styles.h1}>3000+ Happy Customers</div>
-            <div className={styles.h2}>Got an amazing result</div>
-            <div className={styles.rating}>
-                <Stack spacing={1}>
-                    <Rating name="size-large" defaultValue={2} size="large" />
-                </Stack>
-            </div>
-            <div className={styles.para}>Its a very effective medicine. You will get good results if you take it continuously for 3 months.</div>
-            <div className={styles.customer}>Lalith Kumar</div>
-        </div >
-    )
-}
+  return (
+    <div className={styles.BuyRightContainer}>
+      <div className={styles.h1}>3000+ Happy Customers</div>
+      <div className={styles.h2}>Got an amazing result</div>
+      <div className={styles.rating}>
+        <Stack spacing={1}>
+          <Rating name="size-large" defaultValue={2} size="large" />
+        </Stack>
+      </div>
+      <div className={styles.para}>
+        Its a very effective medicine. You will get good results if you take it
+        continuously for 3 months.
+      </div>
+      <div className={styles.customer}>Lalith Kumar</div>
+    </div>
+  );
+};
 
-export default BuyRight
+export default BuyRight;
