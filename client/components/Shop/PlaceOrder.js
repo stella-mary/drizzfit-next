02e89c6b0cabd76fd27e8e1fe6dfb9515811e9bb
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-08-30 17:14:24 +0530

    Update PlaceOrder.js

--- client/components/Shop/PlaceOrder.js
+++ client/components/Shop/PlaceOrder.js
@@ -6,164 +6,173 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Typography from "@mui/material/Typography";
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+const steps = ["Mobile", "Address", "Payment"];
 
 export default function PlaceOrder() {
-    const [openDialog, setOpenDialog] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
 
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [skipped, setSkipped] = React.useState(new Set());
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set());
 
-    const isStepOptional = (step) => {
-        return step === 1;
-    };
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
 
-    const isStepSkipped = (step) => {
-        return skipped.has(step);
-    };
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
 
-    const handleNext = () => {
-        let newSkipped = skipped;
-        if (isStepSkipped(activeStep)) {
-            newSkipped = new Set(newSkipped.values());
-            newSkipped.delete(activeStep);
-        }
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
 
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
-    };
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
 
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
-    const handleSkip = () => {
-        if (!isStepOptional(activeStep)) {
-            // You probably want to guard against something like this,
-            // it should never occur unless someone's actively trying to break something.
-            throw new Error("You can't skip a step that isn't optional.");
-        }
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
 
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped((prevSkipped) => {
-            const newSkipped = new Set(prevSkipped.values());
-            newSkipped.add(activeStep);
-            return newSkipped;
-        });
-    };
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
 
-    const handleReset = () => {
-        setActiveStep(0);
-    };
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
+  const handleBuyNow = () => {
+    // ...existing code...
 
-    const handleBuyNow = () => {
-        // ...existing code...
+    setOpenDialog(true);
+  };
 
-        setOpenDialog(true);
-    };
+  const handleCloseDialog = () => {
+    // Close the dialog
+    setOpenDialog(false);
+  };
 
-    const handleCloseDialog = () => {
-        // Close the dialog
-        setOpenDialog(false);
-    };
-
-    return (
-        <div className={styles.shop}>
-            {/* ...existing code... */}
-            <Button
-                variant="contained"
-                color="primary"
-                sx={{
-                    fontSize: "12px",
-                    fontFamily: "'Telegraf UltraBold 800', sans-serif",
-                    textTransform: "none",
-                    backgroundColor: "black",
-                    color: "white",
-                }}
-                onClick={handleBuyNow}
+  return (
+    <div className={styles.shop}>
+      {/* ...existing code... */}
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{
+          fontSize: "12px",
+          fontFamily: "'Telegraf UltraBold 800', sans-serif",
+          textTransform: "none",
+          backgroundColor: "black",
+          color: "white",
+        }}
+        onClick={handleBuyNow}
+      >
+        place order
+      </Button>
+      {/* Dialog for Order Confirmation */}
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        {/* <DialogTitle>Order Placed Successfully</DialogTitle> */}
+        <DialogContent>
+          <DialogContentText>
+            <Box
+              sx={
+                {
+                  // width: '100%',
+                  // margin: '0 auto'
+                }
+              }
             >
-                place order
-            </Button>
-            {/* Dialog for Order Confirmation */}
-            <Dialog open={openDialog} onClose={handleCloseDialog}>
-                {/* <DialogTitle>Order Placed Successfully</DialogTitle> */}
-                <DialogContent>
-                    <DialogContentText>
-                        <Box sx={{
-                            // width: '100%',
-                            // margin: '0 auto'
-                        }}>
-                            <Stepper activeStep={activeStep}>
-                                {steps.map((label, index) => {
-                                    const stepProps = {};
-                                    const labelProps = {};
-                                    if (isStepOptional(index)) {
-                                        labelProps.optional = (
-                                            <Typography variant="caption">Optional</Typography>
-                                        );
-                                    }
-                                    if (isStepSkipped(index)) {
-                                        stepProps.completed = false;
-                                    }
-                                    return (
-                                        <Step key={label} {...stepProps}>
-                                            <StepLabel {...labelProps}>{label}</StepLabel>
-                                        </Step>
-                                    );
-                                })}
-                            </Stepper>
-                            {activeStep === steps.length ? (
-                                <React.Fragment>
-                                    <Typography sx={{ mt: 2, mb: 1 }}>
-                                        All steps completed - you&apos;re finished
-                                    </Typography>
-                                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                                        <Box sx={{ flex: '1 1 auto' }} />
-                                        <Button onClick={handleReset}>Reset</Button>
-                                    </Box>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-                                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                                        <Button
-                                            color="inherit"
-                                            disabled={activeStep === 0}
-                                            onClick={handleBack}
-                                            sx={{ mr: 1 }}
-                                        >
-                                            Back
-                                        </Button>
-                                        <Box sx={{ flex: '1 1 auto' }} />
-                                        {isStepOptional(activeStep) && (
-                                            <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                                                Skip
-                                            </Button>
-                                        )}
-
-                                        <Button onClick={handleNext}>
-                                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                        </Button>
-                                    </Box>
-                                </React.Fragment>
-                            )}
-                        </Box>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseDialog} color="primary">
-                        Close
+              <Stepper activeStep={activeStep}>
+                {steps.map((label, index) => {
+                  const stepProps = {};
+                  const labelProps = {};
+                  //   if (isStepOptional(index)) {
+                  //     labelProps.optional = (
+                  //       <Typography variant="caption">Optional</Typography>
+                  //     );
+                  //   }
+                  //   if (isStepSkipped(index)) {
+                  //     stepProps.completed = false;
+                  //   }
+                  return (
+                    <Step key={label} {...stepProps}>
+                      <StepLabel {...labelProps}>{label}</StepLabel>
+                    </Step>
+                  );
+                })}
+              </Stepper>
+              {activeStep === steps.length ? (
+                <React.Fragment>
+                  <Typography sx={{ mt: 2, mb: 1 }}>
+                    All steps completed - you&apos;re finished
+                  </Typography>
+                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                    <Box sx={{ flex: "1 1 auto" }} />
+                    <Button onClick={handleReset}>Reset</Button>
+                  </Box>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <Typography sx={{ mt: 2, mb: 1 }}>
+                    Step {activeStep + 1}
+                  </Typography>
+                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                    <Button
+                      color="inherit"
+                      disabled={activeStep === 0}
+                      onClick={handleBack}
+                      sx={{ mr: 1 }}
+                    >
+                      Back
                     </Button>
-                </DialogActions>
-            </Dialog>
-            {/* ...existing code... */}
-        </div>
-    );
+                    <Box sx={{ flex: "1 1 auto" }} />
+                    {isStepOptional(activeStep) && (
+                      <Button
+                        color="inherit"
+                        onClick={handleSkip}
+                        sx={{ mr: 1 }}
+                      >
+                        Skip
+                      </Button>
+                    )}
+
+                    <Button onClick={handleNext}>
+                      {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    </Button>
+                  </Box>
+                </React.Fragment>
+              )}
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* ...existing code... */}
+    </div>
+  );
 }
