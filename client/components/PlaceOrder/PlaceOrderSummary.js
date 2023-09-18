f5bf6b18a9ebd51ecf320d489e3bedde3bc633aa
stella-mary <mariastellamary@outlook.com>
2023-09-18 19:47:35 +0530

    updated placeordersummary.js

--- client/components/PlaceOrder/PlaceOrderSummary.js
+++ client/components/PlaceOrder/PlaceOrderSummary.js
@@ -1,21 +1,58 @@
 import styles from "@/styles/PlaceOrderSummary.module.css";
-import React, { useEffect, useState, useRef } from "react";
-
+import React, { useState } from "react";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const PlaceOrderSummary = () => {
+    const [isContentVisible, setContentVisible] = useState(true);
+
+    const toggleContent = () => {
+        setContentVisible(!isContentVisible);
+    };
+
     return (
-        <div className={styles.BuyDetailsMain}>
-            <div className={styles.imageContainer7}></div>
-            <div className={styles.BuyName}>
-                <div className={styles.BuyNameMain}>
-                    <div className={styles.h1}>Menstrual Cap - Large</div>
-                    <div className={styles.para}>Price: $445</div>
-                    <div className={styles.para}>Quantity: 3</div>
-                </div>
+        <div className={styles.PlaceOrderSummaryHead}>
+            <div className={styles.h1}>
+                <AddShoppingCartIcon style={{ verticalAlign: 'middle' }} />
+                <span className={styles.space} />Order Summary<span className={styles.space} />
+                {isContentVisible ? (
+                    <KeyboardArrowUpIcon onClick={toggleContent} style={{ verticalAlign: 'middle', cursor: 'pointer' }} />
+                ) : (
+                    <KeyboardArrowDownIcon onClick={toggleContent} style={{ verticalAlign: 'middle', cursor: 'pointer' }} />
+                )}
             </div>
+            {isContentVisible && (
+                <div>
+                    <div className={styles.PlaceOrderSummary}>
+                        <div className={styles.imageContainer7}></div>
+                        <div className={styles.PlaceOrderSummaryMain}>
+                            <div className={styles.PlaceOrderSummarySub}>
+                                <div className={styles.h2}>Menstrual Cap - Large</div>
+                                <div className={styles.para}>Price: $445</div>
+                                <div className={styles.para}>Quantity: 3</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.PlaceOrderSummaryTable}>
+                        <div className={styles.PlaceOrderSummaryTable1}>
+                            <div className={styles.thead}>Subtotal</div>
+                            <div className={styles.thead}>₹</div>
+                        </div>
+                        <div className={styles.PlaceOrderSummaryTable1}>
+                            <div className={styles.thead}>Shipping</div>
+                            <div className={styles.thead}>Free</div>
+                        </div>
+                        <div className={styles.border}></div>
+                        <div className={styles.PlaceOrderSummaryTable1}>
+                            <div className={styles.thead1}>To Pay</div>
+                            <div className={styles.thead1}>₹</div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
+    );
+};
 
-    )
-}
-
-export default PlaceOrderSummary
+export default PlaceOrderSummary;
