283553a7ee4dbe766fdc085bf2186254f6ad8015
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-09-21 17:58:55 +0530

    Default Input Corrected

--- client/components/Shop/Shop2.js
+++ client/components/Shop/Shop2.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState, useRef } from "react";
 import styles from "@/styles/Shop1.module.css";
 import axios from "axios";
 import { useMediaQuery } from "@mui/material";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import BuyPage from "@/pages/buy";
 import BuyDetails from "../Buy/BuyDetails";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 
 const Shop2 = () => {
   const [selectedQuantity, setSelectedQuantity] = useState(1);
@@ -15,7 +15,7 @@ const Shop2 = () => {
   const [selectedProduct, setSelectedProduct] = useState({});
   const [selectedDescription, setSelectedDescription] = useState("");
   const [state, setState] = useState({ right: true });
-  const [size, setSize] = useState();
+  const [size, setSize] = useState("Medium");
   const isMobile = useMediaQuery("(max-width: 768px)");
   const [discountCode, setDiscountCode] = useState("");
   const router = useRouter();
@@ -43,11 +43,13 @@ const Shop2 = () => {
 
       // Assuming the first product in the first group is the selected product
       if (groupedProducts[Object.keys(groupedProducts)[0]]) {
-        setSelectedProduct(
-          groupedProducts[Object.keys(groupedProducts)[0]][0]
+        console.log(
+          "Selected product: ",
+          groupedProducts[Object.keys(groupedProducts)[0]][1]
         );
+        setSelectedProduct(groupedProducts[Object.keys(groupedProducts)[0]][1]);
         setSelectedDescription(
-          groupedProducts[Object.keys(groupedProducts)[0]][0].description
+          groupedProducts[Object.keys(groupedProducts)[0]][1].description
         );
       }
     });
@@ -56,16 +58,12 @@ const Shop2 = () => {
   const handleDescriptionChange = (event) => {
     const newDescription = event.target.value;
     setSelectedDescription(newDescription);
-
-    // Find the corresponding product based on the selected description
     const selectedProductGroup = groupedProductDetails[selectedProduct.name];
     const selectedProductWithDescription = selectedProductGroup.find(
       (product) => product.description === newDescription
     );
-
     setSelectedProduct(selectedProductWithDescription);
   };
-
 
   return (
     <div className={styles.shop1Sub}>
@@ -74,38 +72,54 @@ const Shop2 = () => {
       <div className={styles.footnote}>* inclusive of all taxes</div>
       <div className={styles.size}>
         {groupedProductDetails[selectedProduct.name]?.map((product) => (
-          <label
-            key={product._id}
-            className={styles.customfont}
-          >
+          <label key={product._id} className={styles.customfont}>
             <input
               type="radio"
               value={product.description}
               name="productDescription"
-              style={{ marginRight: '5px' }}
+              style={{ marginRight: "5px" }}
               onChange={handleDescriptionChange}
-              defaultChecked={product.description === size} />
-            <span style={{ verticalAlign: 'middle' }}>{product.description}</span>
+              defaultChecked={product.description === size}
+            />
+            <span style={{ verticalAlign: "middle" }}>
+              {product.description}
+            </span>
           </label>
         ))}
       </div>
       <div className={styles.boxnote}>
         <div className={styles.boxnoteh1}>What does this pack contain ?</div>
-        <div className={styles.boxnotePara}>1 Menstrual Cup with Cotton Pouch</div>
-        <div className={styles.boxnotePara}>The Drizzfit Menstrual Cup is a perfect option for you to go all
-          sustainable during your cycle. Our cups are:</div>
+        <div className={styles.boxnotePara}>
+          1 Menstrual Cup with Cotton Pouch
+        </div>
+        <div className={styles.boxnotePara}>
+          The Drizzfit Menstrual Cup is a perfect option for you to go all
+          sustainable during your cycle. Our cups are:
+        </div>
         <div className={styles.list}>
           <li>100 % Medical Grade Silicone</li>
           <li>BPA Free | Latex Free</li>
           <li>Made in FDA Approved Plant</li>
           <li>Reusable upto 8 years</li>
         </div>
-        <div className={styles.boxnotePara}><b>Size Guide</b></div>
-        <div className={styles.boxnotePara}>Drizzfit Menstrual Cups comes in 3 sizes</div>
-        <div className={styles.boxnotePara}><b>Small(S)</b> – Perfect if you are under 18 years of age</div>
-        <div className={styles.boxnotePara1}><b>Mediumn (M)</b> – Perfect if you are above 18 years // or gave birth via C - section</div>
-        <div className={styles.boxnotePara1}><b>Large (L)</b> – Perfect if you have given birth vaginally</div>
-      </div> <div className={styles.shop1SubSub}>
+        <div className={styles.boxnotePara}>
+          <b>Size Guide</b>
+        </div>
+        <div className={styles.boxnotePara}>
+          Drizzfit Menstrual Cups comes in 3 sizes
+        </div>
+        <div className={styles.boxnotePara}>
+          <b>Small(S)</b> – Perfect if you are under 18 years of age
+        </div>
+        <div className={styles.boxnotePara1}>
+          <b>Mediumn (M)</b> – Perfect if you are above 18 years // or gave
+          birth via C - section
+        </div>
+        <div className={styles.boxnotePara1}>
+          <b>Large (L)</b> – Perfect if you have given birth vaginally
+        </div>
+      </div>{" "}
+      <div className={styles.shop1SubSub}>
         <input
           type="number"
           id="quantity"
@@ -138,7 +152,10 @@ const Shop2 = () => {
           <div className={styles.closeButton} onClick={closeSidebar}>
             <CloseIcon />
           </div>
-          <BuyPage selectedQuantity={selectedQuantity} setSelectedQuantity={setSelectedQuantity} />
+          <BuyPage
+            selectedQuantity={selectedQuantity}
+            setSelectedQuantity={setSelectedQuantity}
+          />
         </div>
       )}
     </div>
