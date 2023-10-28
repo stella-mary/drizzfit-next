40c371e90f8a6999389729e33b1f1648ca8cfbb0
stella-mary <mariastellamary@outlook.com>
2023-10-28 11:49:30 +0530

    Update PlaceOrderPayment.js

--- client/components/PlaceOrder/PlaceOrderPayment.js
+++ client/components/PlaceOrder/PlaceOrderPayment.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PlaceOrderPayment = ({ mobileNumber }) => {
     return (
         <div>
-            {mobileNumber}
+            Please select payment method to complete the order
         </div>
     )
 }
