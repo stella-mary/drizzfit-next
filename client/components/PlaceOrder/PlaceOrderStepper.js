046175de7d1355c9b1bd8e4ba360facb17c1dbee
stella-mary <mariastellamary@outlook.com>
2023-11-02 15:15:41 +0530

    updated

--- client/components/PlaceOrder/PlaceOrderStepper.js
+++ client/components/PlaceOrder/PlaceOrderStepper.js
@@ -1,20 +1,26 @@
 import React from "react";
 import styles from "@/styles/PlaceOrderStepper.module.css";
-import CheckIcon from "@mui/icons-material/Check"; // Import the closed-tick icon
+import CheckIcon from "@mui/icons-material/Check";
 
 const PlaceOrderStepper = ({ activeStep }) => {
   return (
     <div className={styles["place-order-stepper"]}>
       <div className={activeStep >= 1 ? styles.active : styles.step}>
-        <span className={styles.bgcolor}>{activeStep <= 1 ? 1 : <CheckIcon />}</span>
+        <span className={activeStep <= 1 ? styles.bgcolor : styles.checkIcon}>
+          {activeStep <= 1 ? 1 : <CheckIcon />}
+        </span>
         <span className={styles.space}></span>Mobile
       </div>
       <div className={activeStep >= 2 ? styles.active : styles.step}>
-        <span className={styles.bgcolor}>{activeStep <= 2 ? 2 : <CheckIcon />}</span>
+        <span className={activeStep <= 2 ? styles.bgcolor : styles.checkIcon}>
+          {activeStep <= 2 ? 2 : <CheckIcon />}
+        </span>
         <span className={styles.space}></span>Address
       </div>
       <div className={activeStep >= 3 ? styles.active : styles.step}>
-        <span className={styles.bgcolor}>{activeStep <= 3 ? 3 : <CheckIcon />}</span>
+        <span className={activeStep <= 3 ? styles.bgcolor : styles.checkIcon}>
+          {activeStep <= 3 ? 3 : <CheckIcon />}
+        </span>
         <span className={styles.space}></span>Payment
       </div>
     </div>
