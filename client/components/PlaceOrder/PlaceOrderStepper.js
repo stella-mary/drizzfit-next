a4ae46738bb7c41df21e1082a8b5971c75bccdf6
stella-mary <mariastellamary@outlook.com>
2023-11-03 18:17:27 +0530

    updated stepper.js

--- client/components/PlaceOrder/PlaceOrderStepper.js
+++ client/components/PlaceOrder/PlaceOrderStepper.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "@/styles/PlaceOrderStepper.module.css";
 import CheckIcon from "@mui/icons-material/Check";
 
 const PlaceOrderStepper = ({ activeStep }) => {
   return (
     <div className={styles["place-order-stepper"]}>
-      <div className={activeStep === 1 ? styles.active : styles.step}>
-        <span className={activeStep === 1 ? styles.bgcolor : styles.checkIcon}>
-          {activeStep === 1 ? 1 : <CheckIcon />}
+      <div className={activeStep >= 1 ? styles.active : styles.step}>
+        <span className={activeStep <= 1 ? styles.bgcolor : styles.checkIcon}>
+          {activeStep <= 1 ? 1 : <CheckIcon />}
         </span>
         <span className={styles.space}></span>Mobile
       </div>
-      <div className={activeStep === 2 ? styles.active : styles.step}>
-        <span className={activeStep === 2 ? styles.checkIcon : styles.bgcolor}>
-          {activeStep === 2 ? 2 : <CheckIcon />}
+      <div className={activeStep >= 2 ? styles.disabled : styles.step}>
+        <span className={activeStep <= 2 ? styles.bgcolor : styles.checkIcon}>
+          {activeStep <= 2 ? 2 : <CheckIcon />}
         </span>
         <span className={styles.space}></span>Address
       </div>
-      <div className={activeStep === 3 ? styles.active : styles.step}>
-        <span className={activeStep === 3 ? styles.checkIcon : styles.bgcolor}>
-          {activeStep === 3 ? 3 : <CheckIcon />}
+      <div className={activeStep >= 3 ? styles.disabled : styles.step}>
+        <span className={activeStep <= 3 ? styles.bgcolor : styles.checkIcon}>
+          {activeStep <= 3 ? 3 : <CheckIcon />}
         </span>
         <span className={styles.space}></span>Payment
       </div>
