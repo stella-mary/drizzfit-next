5ca35daf2dbdd6f4ae2d6457f671672c54c5ec11
stella-mary <mariastellamary@outlook.com>
2023-11-04 17:30:41 +0530

    updated stepper.js

--- client/components/PlaceOrder/PlaceOrderStepper.js
+++ client/components/PlaceOrder/PlaceOrderStepper.js
@@ -5,25 +5,28 @@ import CheckIcon from "@mui/icons-material/Check";
 const PlaceOrderStepper = ({ activeStep }) => {
   return (
     <div className={styles["place-order-stepper"]}>
-      <div className={activeStep >= 1 ? styles.disabled : <span className="styles.bgcolor" />}>
-        <span className={activeStep <= 1 ? styles.bgcolor : styles.checkIcon}>
+      <div className={activeStep >= 1 ? styles.disabled : <span className={styles.bgcolor} />}>
+        <span className={activeStep <= 1 ? styles.bgcolor1 : styles.checkIcon}>
           {activeStep <= 1 ? 1 : <CheckIcon />}
         </span>
-        <span className={styles.space}></span>Mobile
+        <span className={styles.space}></span>
+        <span className={activeStep <= 1 ? styles.title : styles.title1}>Mobile</span>
       </div>
-      <div className={activeStep >= 2 ? styles.disabled : <span className="styles.bgcolor" />}>
-        <span className={activeStep <= 2 ? styles.bgcolor : styles.checkIcon}>
+      <div className={activeStep >= 2 ? styles.disabled : <span className={styles.bgcolor} />}>
+        <span className={activeStep <= 2 ? styles.bgcolor1 : styles.checkIcon}>
           {activeStep <= 2 ? 2 : <CheckIcon />}
         </span>
-        <span className={styles.space}></span>Address
+        <span className={styles.space}></span>
+        <span className={activeStep <= 2 ? styles.title : ''}>Address</span>
       </div>
-      <div className={activeStep >= 3 ? styles.disabled : <span className="styles.bgcolor" />}>
-        <span className={activeStep <= 3 ? styles.bgcolor : styles.checkIcon}>
+      <div className={activeStep >= 3 ? styles.disabled : <span className={styles.bgcolor} />}>
+        <span className={activeStep <= 3 ? styles.bgcolor1 : styles.checkIcon}>
           {activeStep <= 3 ? 3 : <CheckIcon />}
         </span>
-        <span className={styles.space}></span>Payment
+        <span className={styles.space}></span>
+        <span className={activeStep <= 3 ? styles.title : ''}>Payment</span>
       </div>
-    </div>
+    </div >
   );
 };
 
